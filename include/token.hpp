8bff1b5581734b102a0ed20bdd44c1337beaff68
otreblan <otreblain@gmail.com>
2020-10-08 18:14:36 -0500

    Add token

---
+++ include/token.hpp
@@ -0,0 +1,41 @@
+// Operation Solver
+// Copyright © 2020 otreblan
+//
+// osol is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osol is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with osol.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace aru
+{
+
+enum class token_type
+{
+	CHAR,
+	INT,
+	STRING
+};
+
+struct token
+{
+	token_type type;
+
+	union value
+	{
+		char c;
+		int i;
+		char* str_ref;
+	};
+};
+
+};
